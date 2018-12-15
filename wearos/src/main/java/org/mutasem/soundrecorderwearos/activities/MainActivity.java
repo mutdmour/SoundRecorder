95491b8e76c07a5f0817be34c38a109f7d3893a0
mutdmour <mutdmour@gmail.com>
2018-12-15 14:17:38 +0200

    feature/wearos some basic attempt, update gradle

---
+++ wearos/src/main/java/org/mutasem/soundrecorderwearos/activities/MainActivity.java
@@ -0,0 +1,26 @@
+package org.mutasem.soundrecorderwearos.activities;
+package com.danielkim.soundrecorder.activities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+//import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+//import com.astuetz.PagerSlidingTabStrip;
+//import com.danielkim.soundrecorder.R;
+//import com.danielkim.soundrecorder.fragments.FileViewerFragment;
+//import com.danielkim.soundrecorder.fragments.LicensesFragment;
+import org.mutasem.soundrecorderwearos.fragments.RecordFragment;
+public class MainActivity extends {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private ViewPager pager;
+
+}
